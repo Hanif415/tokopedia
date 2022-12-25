8601d2fcbb04cac6922bf6e5134aac6bfae20d04
hanif415 <hanifamrullah42@gmail.com>
2022-12-25 16:58:14 +0700

    add registter inde and finish it

---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tokopedia</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
+        {{-- Bootstrap Icons --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    </head>
+    <body>
+        <div class="container mb-4">
+            <div class="position-absolute mt-5 top-0 start-50 translate-middle">
+                <a href="/"><img class="" width="170" src="img/Tokopedia.png" alt=""></a>
+            </div>
+            <div class="row p-5" style="margin-top: 10%">
+                <div class="col">
+                    <img src="https://ecs7.tokopedia.net/img/content/register_new.png" alt="tokopedia" style="width: 380px">
+                    <h3>Discover Millions of Trusted Shops</h3>
+                    <p class=" ms-5 text-secondary">Discover Millions of Trusted Shops</p>
+                </div>
+                <div class="col p-3 shadow-sm">
+                    <main class="form-signin m-auto">
+                        <form>
+                            <div class="">
+                                <h1 class="h3 mb-3 font-weight-bolder d-flex justify-content-center">Sign Up now</h1>
+                                <p class="text-secondary d-flex justify-content-center">Already have a Tokopedia account?<a href="login" class="text-success text-decoration-none"> Log In</a></p>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                <input type="email" class="form-control form-control-md" id="exampleFormControlInput1" placeholder="name@example.com">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="floatingPassword">Password</label>
+                                <input type="password" class="form-control form-control-md" id="floatingPassword" placeholder="Password">
+                            </div>
+
+                            <button class="w-100 btn btn-lg btn-success" type="submit">Login</button>
+                        </form>
+                    </main>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
