fa09be3cae1111d32d99dee104c3d5a102c290ac
hanif415 <hanifamrullah42@gmail.com>
2022-12-25 16:59:39 +0700

    add login modal blade

---
+++ resources/views/login/login-modal.blade.php
@@ -0,0 +1,37 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="container p-3">
+                <div class="modal-body">
+                    <main class="form-signin w-60 m-auto">
+                        <form>
+                            <div class="row">
+                                <div class="col-9">
+                                    <h1 class="h3 mb-3 fw-bold">Login</h1>
+                                </div>
+                                <div class="col">
+                                    <a href="register" class=" text-decoration-none text-success justify-content-end">Sign Up</a>
+                                </div>
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                <input type="email" class="form-control form-control-md" id="exampleFormControlInput1" placeholder="name@example.com">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="floatingPassword">Password</label>
+                                <input type="password" class="form-control form-control-md" id="floatingPassword" placeholder="Password">
+                            </div>
+
+                            <button class="w-100 btn btn-lg btn-success" type="submit">Login</button>
+                        </form>
+                    </main>
+                </div>
+            </div>
+    </div>
+    </div>
+</div>
