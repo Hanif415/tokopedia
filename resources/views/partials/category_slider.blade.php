bde3d68881efe35eeb383d11ca9646b9962d5751
hanif415 <hanifamrullah42@gmail.com>
2022-12-25 16:59:16 +0700

    add category slider blade

---
+++ resources/views/partials/category_slider.blade.php
@@ -0,0 +1,45 @@
+<h5>Kategori pilihan</h5>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="cards-wrapper">
+                <div class="card">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_e5b3e074-c897-4cf0-9ced-5572d0538e7c.jpg.webp" class="card-img-top" alt="...">
+                </div>
+                <div class="card d-none d-md-block">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_c6f70915-577f-4cd4-834c-daf892265ef0.jpg.webp" class="card-img-top" alt="...">
+                </div>
+                <div class="card d-none d-md-block">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_d7c67b76-9ff4-46f9-93a3-ec4be4918439.jpg.webp" class="card-img-top" alt="...">
+                </div>
+                <div class="card">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_76d08c44-8194-4454-9fb0-1e7a64b656aa.jpg.webp" class="card-img-top" alt="...">
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="cards-wrapper">
+                <div class="card">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_76d08c44-8194-4454-9fb0-1e7a64b656aa.jpg.webp" class="card-img-top" alt="...">
+                </div>
+                <div class="card d-none d-md-block">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_cfe03d9c-d79f-4327-8247-458429e1b116.jpg.webp" class="card-img-top" alt="...">
+                </div>
+                <div class="card d-none d-md-block">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/11/3127195/3127195_2883bb63-8f91-4c9c-a423-d68b22e51b78.jpg.webp" class="card-img-top" alt="...">
+                </div>
+                <div class="card">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/attachment/2018/8/9/3127195/3127195_e5b3e074-c897-4cf0-9ced-5572d0538e7c.jpg.webp" class="card-img-top" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
