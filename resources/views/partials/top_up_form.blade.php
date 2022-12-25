3b2566f0c6d4b183b076de4b0618cd8abb991216
hanif415 <hanifamrullah42@gmail.com>
2022-12-25 16:58:30 +0700

    add top up blade

---
+++ resources/views/partials/top_up_form.blade.php
@@ -0,0 +1,26 @@
+<h5>Top Up & Tagihan</h5>
+<div class="shadow-sm p-3 rounded">
+    <ul class="nav nav-tabs justify-content-between" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active text-success" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Pulsa</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link text-success" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Paket Data</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link text-success" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Flight</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link text-success" id="listrik-tab" data-bs-toggle="tab" data-bs-target="#listrik-tab-pane" type="button" role="tab" aria-controls="listrik-tab-pane" aria-selected="false">Listrik PLN</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="disabled-tab" data-bs-toggle="tab" data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane" aria-selected="false" disabled><a href=""><span class="bi bi-three-dots-vertical"></span></a></button>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae repudiandae earum nobis magni voluptate a deserunt adipisci saepe aspernatur officiis.</div>
+        <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae repudiandae earum nobis magni voluptate a deserunt adipisci saepe aspernatur officiis.</div>
+        <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae repudiandae earum nobis magni voluptate a deserunt adipisci saepe aspernatur officiis.</div>
+        <div class="tab-pane fade" id="listrik-tab-pane" role="tabpanel" aria-labelledby="listrik-tab" tabindex="0">    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae repudiandae earum nobis magni voluptate a deserunt adipisci saepe aspernatur officiis.</div>
+    </div>
+</div>
