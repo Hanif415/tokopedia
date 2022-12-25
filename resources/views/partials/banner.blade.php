8c2205c7874797fccfeaf67c2844fdc11b4cc11a
hanif415 <hanifamrullah42@gmail.com>
2022-12-25 16:59:26 +0700

    add banner blade

---
+++ resources/views/partials/banner.blade.php
@@ -0,0 +1,22 @@
+<div id="carouselExampleIndicators" class="carousel slide mb-3" data-bs-ride="true">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/12/15/405e792a-eacb-4a05-b066-54ef4f5e2d9f.jpg.webp?ect=4g" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/12/14/d923f234-c5f8-421c-9006-134b7bd3f25f.jpg.webp?ect=4g" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
