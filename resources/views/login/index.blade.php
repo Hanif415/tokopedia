ba5d7b19962e2381a007aa0d484aea9d0c0dfd7b
hanif415 <hanifamrullah42@gmail.com>
2022-12-25 16:59:52 +0700

    add login index blade

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tokopedia</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
+        {{-- Bootstrap Icons --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    </head>
+    <body>
+        <div class="container mb-4">
+            <div class="position-absolute mt-5 top-0 start-50 translate-middle">
+                <a href="/"><img class="" width="170" src="img/Tokopedia.png" alt=""></a>
+            </div>
+            <img class="position-absolute start-50 translate-middle" src="https://assets.tokopedia.net/assets-tokopedia-lite/v2/zeus/kratos/45ab29df.png" alt="background-image" style="width:60%;  z-index: -1; margin-top:20%">
+            <div class="p-lg-5" style="margin-top:10%;">
+                <main class="form-signin w-50 m-auto p-5">
+                    <form>
+                        <div class="row">
+                            <div class="col-9">
+                                <h1 class="h3 mb-3 fw-bold">Login</h1>
+                            </div>
+                            <div class="col">
+                                <a href="register" class="text-decoration-none text-success">Sign Up</a>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                            <input type="email" class="form-control form-control-md" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="floatingPassword">Password</label>
+                            <input type="password" class="form-control form-control-md" id="floatingPassword" placeholder="Password">
+                        </div>
+
+                        <button class="w-100 btn btn-lg btn-success" type="submit">Login</button>
+                    </form>
+                </main>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
