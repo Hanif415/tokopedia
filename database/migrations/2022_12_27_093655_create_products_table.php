632cdb70d77aee8190e955b00c622427f754a6fa
hanif415 <hanifamrullah42@gmail.com>
2022-12-27 17:00:35 +0700

    add migrations

--- database/migrations/2022_12_27_093655_create_products_table.php
+++ database/migrations/2022_12_27_093655_create_products_table.php
@@ -13,13 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $tbale->string('phone_number');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('product_name');
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
